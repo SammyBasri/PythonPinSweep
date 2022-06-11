8316513726b229073e61acc27d07f93e067b18ff
SammyBasri <89367242+SammyBasri@users.noreply.github.com>
2022-06-11 11:17:25 -0400

    Create pysweep.py

---
+++ pysweep.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+#Usage: python3 <First 3 octets i.e 10.0.0> <First forth octet> <Last forth octet> 
+import os 
+import sys
+
+a=sys.argv[1]
+b=int(sys.argv[2])
+c=int(sys.argv[3])
+
+for x in range(b, c):
+        
+        d =  (a + "." + str(x))
+
+        response = os.system("ping %s -c 1 > /dev/null" %d)
+        
+        if response == 0:
+                print("%s" %d)
